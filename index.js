93abf72b8fb1a77a65af4a145b6fb8e7ac715128
doug <dougbostick923@gmail.com>
2024-03-21 18:34:30 -0400

    added post route for openai queries

--- index.js
+++ index.js
@@ -4,54 +4,51 @@ const dotenv = require('dotenv').config()
 const app = express();
 const { Configuration, OpenAI } = require('openai');
 
-// const newConfig = new Configuration({
-//   apiKey: dotenv.parsed.KEY
-// })
 
 const openai = new OpenAI()
 
-console.log('dotenv', dotenv.parsed.OPENAI_API_KEY)
-
 app.use(cors());
+app.use(express.json())
 
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
+  res.send('Hello from express!')
+})
+
+app.post('/gpt', async (req, res, next) => {
+  console.log('REQ', req.body)
+  const ingredients = req.body.ingredientList;
   try{
-
-    const GPTOutpt = await openai.chat.completions.create({
-      messages: [{'role': 'user', 'content': 'Say this is a test!'}],
+      const GPTOutpt = await openai.chat.completions.create({
+      messages: [
+        {'role' : 'system', 'content': 'You are a helpful assistant designed to suggest recipes based on available ingredients.'},
+        {'role': 'user', 'content': `What kind of recipe can I make with these ingredients? ${ingredients}`},
+      ],
       model: 'gpt-3.5-turbo',
     })
 
-    const text = GPTOutpt.choices[0];
-
-    console.log('text', text)
+    console.log('GPTOUT', GPTOutpt)
+    const text = GPTOutpt.choices[0].message.content;
 
     res.send(text)
-    // await fetch('https://api.openai.com/v1/chat/completions', {
-    //   method: 'post',
-    //   headers: {
-    //     'Authorization': `Bearer ${dotenv.parsed.KEY}`
-    //   }
-    // }).then(response => response.json())
-    // .then(json => res.send(json))
+
   } catch(err){
-    console.log(err)
+    next(err)
   }
 });
 
-app.get('/gpt', async(req, res) => {
-  try{
-    await fetch('https://api.openai.com/v1/chat/completions', {
-      method: 'get',
-      headers: {
-        'Authorization': `Bearer ${dotenv.parsed.KEY}`
-      }
-    }).then(response => response.json())
-    .then(json => res.send(json))
-  } catch(err){
-    console.log(err)
-  }
-})
+// app.get('/gpt', async(req, res) => {
+//   try{
+//     await fetch('https://api.openai.com/v1/chat/completions', {
+//       method: 'get',
+//       headers: {
+//         'Authorization': `Bearer ${dotenv.parsed.KEY}`
+//       }
+//     }).then(response => response.json())
+//     .then(json => res.send(json))
+//   } catch(err){
+//     console.log(err)
+//   }
+// })
 
 app.listen(3001, () => {
     console.log('Express server listening on port 3001')

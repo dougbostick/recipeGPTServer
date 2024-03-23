d4fd8f6e705afd1bb0b9f0eac8d273de2dbe358a
doug <dougbostick923@gmail.com>
2024-03-23 18:10:11 -0400

    json formatting of gpt response

--- index.js
+++ index.js
@@ -17,18 +17,27 @@ app.get('/', (req, res) => {
 app.post('/gpt', async (req, res, next) => {
   console.log('REQ', req.body)
   const ingredients = req.body.ingredientList;
+  const jsonFormat = `
+    Format the response as an array where each item in the array is a recipe in the following JSON format: 
+    {
+      recipe: <title of recipe>,
+      ingredients: <unordered list of required ingredients>,
+      steps: <ordered list of the steps required to follow recipe>
+    }
+  `
   try{
       const GPTOutpt = await openai.chat.completions.create({
-      messages: [
-        {'role' : 'system', 'content': 'You are a helpful assistant designed to suggest recipes based on available ingredients.'},
-        {'role': 'user', 'content': `What kind of recipe can I make with these ingredients? ${ingredients}`},
-      ],
-      model: 'gpt-3.5-turbo',
-    })
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {'role' : 'system', 'content': `You are a helpful assistant designed to suggest a list of 3-10 recipes based on available ingredients. ${jsonFormat}`},
+          {'role': 'user', 'content': `What kind of recipe can I make with these ingredients? ${ingredients}`},
+        ],
+        response_format: {"type": "json_object"}
+      })
 
     console.log('GPTOUT', GPTOutpt)
     const text = GPTOutpt.choices[0].message.content;
-
+    
     res.send(text)
 
   } catch(err){
@@ -36,19 +45,26 @@ app.post('/gpt', async (req, res, next) => {
   }
 });
 
-// app.get('/gpt', async(req, res) => {
+// app.post('/gptembed', async (req, res, next) => {
+//   console.log('REQ', req.body)
+//   const ingredients = req.body.ingredientList;
 //   try{
-//     await fetch('https://api.openai.com/v1/chat/completions', {
-//       method: 'get',
-//       headers: {
-//         'Authorization': `Bearer ${dotenv.parsed.KEY}`
-//       }
-//     }).then(response => response.json())
-//     .then(json => res.send(json))
+//       const GPTOutpt = await openai.embeddings.create({
+//       model: 'text-embedding-ada-002',
+//       input: `Suggest a recipe based on these ingredients: ${ingredients}.`,
+//       encoding_format: 'float'
+//     })
+
+//     console.log('GPTOUT', GPTOutpt)
+   
+
+//     res.send(GPTOutpt)
+
 //   } catch(err){
-//     console.log(err)
+//     next(err)
 //   }
-// })
+// });
+
 
 app.listen(3001, () => {
     console.log('Express server listening on port 3001')
